2e6b6bc91cf992c7be98fddd601b8af4f54aee30
Dan Collins <dan@collinsnz.com>
2021-05-28 08:01:46 +0000

    Added more blinky

--- src/main.c
+++ src/main.c
@@ -1,13 +1,99 @@
 #include "stm32f7xx_hal.h"
 
 
+extern void Error_Handler(void);
+extern void assert_failed(uint8_t *file, uint32_t line);
+
+
+/* auto-gen from STM cube. */
+void
+SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /* Configure LSE Drive Capability. */
+    HAL_PWR_EnableBkUpAccess();
+
+    /* Configure the main internal regulator output voltage. */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /* Initializes the CPU, AHB and APB busses clocks. */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 25;
+    RCC_OscInitStruct.PLL.PLLN = 432;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 2;
+
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /* Activate the Over-Drive mode. */
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    /* Initializes the CPU, AHB and APB busses clocks. */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_HCLK |
+                                   RCC_CLOCKTYPE_SYSCLK |
+                                   RCC_CLOCKTYPE_PCLK1 |
+                                   RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+
 int
 main(void)
 {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    HAL_Init();
+    SystemClock_Config();
+
+    __HAL_RCC_GPIOI_CLK_ENABLE();
+
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
+
+    HAL_GPIO_WritePin(GPIOI, GPIO_PIN_1, GPIO_PIN_RESET);
+
     while (1)
     {
-        /* ... */
+        HAL_GPIO_WritePin(GPIOI, GPIO_PIN_1, GPIO_PIN_SET);
+        HAL_Delay(500);
+        HAL_GPIO_WritePin(GPIOI, GPIO_PIN_1, GPIO_PIN_RESET);
+        HAL_Delay(500);
     }
 
     return 0;
+}
+
+void
+Error_Handler(void)
+{
+
+}
+
+void
+assert_failed(uint8_t *file, uint32_t line)
+{
+
 }
